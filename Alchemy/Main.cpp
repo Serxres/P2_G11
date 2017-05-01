2c813d961d85363a3a082bee2b66de02d22e3744
Administrador <Sergio.escuder5@gmail.com>
2017-05-01 13:45:07 +0200

    avnace 01/05/17

--- Alchemy/Main.cpp
+++ Alchemy/Main.cpp
@@ -14,6 +14,10 @@ struct combinations
 	std::string second;
 };
 
+//ENUM CLASS PARA EL CONTROL DE OPERACIONES
+enum class Instructions{add,basics,remove,info,sort,clean,help };
+
+
 //FUNCION QUE IMPRIME LAS INSTRUCCIONES
 void printInstructions()
 {
@@ -23,15 +27,38 @@ void printInstructions()
 			"- Enter two number of your elemtns list to combine them.\n" <<
 			"- Enter the word 'add' and a number of an element to add a new instance of that element\n" <<
 			"- Enter 'add basics' to add new instances of the 4 basic elemtns.\n" <<
-			"- Enter the word 'delete' and the number of an element to erase it from your list.\n" <<
+			"- Enter the word 'remove' and the number of an element to erase it from your list.\n" <<
 			"- Enter the word 'info' and the number of an element to get the information about it in the explorer.\n" <<
 			"- Enter the word 'sort' to sort by alphabetical order the elements in the list.\n" <<
 			"- Enter the word 'clean' to delete all the instances of repeated elements.\n" <<
 			"- Enter the word 'help' to show this tutorial." << std::endl;
 }
 
+Instructions castToEnum(std::string word)
+{
+	Instructions casted;
+	if (word == "add")
+		casted = Instructions::add;
+	if (word == "basics")
+		casted = Instructions::basics;
+	if (word == "remove")
+		casted = Instructions::remove;
+	if (word == "info")
+		casted = Instructions::info;
+	if (word == "sort")
+		casted = Instructions::sort;
+	if (word == "clean")
+		casted = Instructions::clean;
+	if (word == "help")
+		casted = Instructions::help;
+	
+
+
+		return casted;
+};
 //FUNCION QUE GUARDA TODA LA LISTA DE ELEMENTOS EN UN UNORDERED_MAP
-void fillListOfElements(std::unordered_map<std::string, std::string> &elementsList)
+/*
+void fillListOfElements(std::unordered_map<std::string, std::string> elementsList)
 { 
 
 	std::ifstream elements("elements.dat");
@@ -44,6 +71,7 @@ void fillListOfElements(std::unordered_map<std::string, std::string> &elementsLi
 	{
 		size_t equalPosition;
 		size_t plusPosition;
+		int sizeKey1;
 		std::string key1;
 		std::string key2;
 		std::string keyTotal;
@@ -52,22 +80,32 @@ void fillListOfElements(std::unordered_map<std::string, std::string> &elementsLi
 		//QUE VAYA LEYENDO MIENTRAS NO SE LLEGUE AL FINAL DEL ARCHIVO
 		while (elements.eof() != true)
 		{
+			//LEEMOS UNA LINEA DEL ARCHIVO Y ENCONTRAMOS LAS POSICIONES 
+			//DEL = Y EL + QUE ES LO QUE NOS SEPARA CADA ELEMENTO
 			getline(elements, line);
 			equalPosition = line.find("=");
 			plusPosition = line.find('+');
-
+			sizeKey1 = (plusPosition - equalPosition) - 3;
+			//SEPARAMOS LA LINEA POR LOS SIMBOLOS ANTES DICHOS
 			value = line.substr(0, equalPosition - 1);
-			key1 = line.substr(equalPosition + 2, plusPosition - 2);
+			key1 = line.substr(equalPosition + 2, sizeKey1);
 			key2 = line.substr(plusPosition + 2, line.size());
+			//GENERAMOS LA KEY QUE SERA LOS DOS ELEMENTOS DE LA COMBINACION EN UN STRING
+			keyTotal = key1 + key2;
+			//LO GUARDAMOS EN NUESTRO UNORDERD_MAP
+			elementsList.insert(keyTotal, value);
+			
 
-
-			std::cout << value << "!" << "\n";
-			std::cout << key1 << "!" << "\n";
-			std::cout << key2 << "!" << std::endl;
 
 		}
 	}
-}
+	else
+	{
+		system("pause");
+	}
+
+	elements.close();
+}*/
 
 
 void main()
@@ -76,34 +114,54 @@ void main()
 
 	std::unordered_map<std::string, std::string> elementsList;
 
-	std::ifstream elements("elements.dat");
-	//STRING DONDE GUARDAREMOS CADA LINEA PARA DESPUES UTILZARLA
-	std::string line;
+	//fillListOfElements(elementsList);
 
-
+	std::vector<std::string> gameList({ "Air","Earth","Fire","Water" });
+	std::vector<std::string> scoreList(gameList);
 	
-		size_t equalPosition;
-		size_t plusPosition;
-		int sizeKey1;
-		std::string key1;
-		std::string key2;
-		std::string keyTotal;
-		std::string value;
+	//VARIABLE QUE CONTROLA LA PUNTUACION
+	int score = 0;
+	//LA PRIMERA Y SEGUNDA PALABRA QUE NOS ESCRIBA EL USUARIO
+	std::string firstWord;
+	std::string secondWord;
+	//EL ENUM CON EL QUE RECORREREMOS EL SWITCH 
+	Instructions order;
 
-		for (int i = 0; i < 10; i++)
+	while (score <= 395)
+	{
+		//GUARDAMOS LOS COMANDOS ELEGIOS POR EL USUARIO
+		std::cin >> firstWord >> secondWord;
+		//PARA PODER HACER EL SWITCH HACEMOS UN CAST DE STRING A NUESTRO ENUM DE ORDENES
+		//CON UNA FUNCION PREVIAMENT CREADA
+		order = castToEnum(firstWord);
+
+		switch (order)
 		{
-			getline(elements, line);
-			equalPosition = line.find("=");
-			plusPosition = line.find('+');
+		case Instructions::add:
 			
-			value = line.substr(0, equalPosition - 1);	
-			key1 = line.substr(equalPosition + 2, plusPosition-2);
-			key2 = line.substr(plusPosition + 2, line.size());
-			
+			break;
+		case Instructions::remove:
 
-			std::cout << value << "!" << "\n";
-			std::cout << key1 << "!" << "\n";
-			std::cout << key2 << "!" << std::endl;
+			break;
+		case Instructions::info:
+
+			break;
+		case Instructions::sort:
+
+			break;
+		case Instructions::clean:
+
+			break;
+		case Instructions::help:
+
+			break;
+		default:
+			break;
 		}
+		std::cout << firstWord << " " << secondWord << std::endl;
+		score++;
+
+	}
+ 
 
 }
